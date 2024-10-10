0317a693726e35f8bc77b178d7c68c1803c0226d
jwatson-ccc <72507396+jwatson-ccc@users.noreply.github.com>
2024-10-10 16:38:06 -0400

    feature: Implement support for loading role configuration (#36)

---
+++ migrate.go
@@ -0,0 +1,148 @@
+// deployment provides the utilities to bootstrap the application with preset configuration
+package access
+
+import (
+	"context"
+	"fmt"
+	"slices"
+
+	"github.com/cccteam/ccc/accesstypes"
+	"github.com/cccteam/ccc/resourcestore"
+	"github.com/go-playground/errors/v5"
+	"go.opentelemetry.io/otel"
+)
+
+type RoleConfig struct {
+	Roles []*Role `json:"roles"`
+}
+
+type Role struct {
+	Name        accesstypes.Role
+	Permissions map[accesstypes.Permission][]accesstypes.Resource
+}
+
+// MigrateRoles runs through and adds the specified roles with specific permissions to the application
+func MigrateRoles(ctx context.Context, client UserManager, store *resourcestore.Store, roleConfig *RoleConfig) error {
+	ctx, span := otel.Tracer(name).Start(ctx, "MigrateRoles()")
+	defer span.End()
+
+	// Default Administrator role has all permissions
+	roleConfig.Roles = append(roleConfig.Roles, &Role{
+		Name:        "Administrator",
+		Permissions: store.List(),
+	})
+
+	if err := bootstrapRoles(ctx, client, store, roleConfig.Roles); err != nil {
+		return errors.Wrap(err, "bootstrapRoles()")
+	}
+
+	return nil
+}
+
+func bootstrapRoles(ctx context.Context, client UserManager, store *resourcestore.Store, roles []*Role) error {
+	ctx, span := otel.Tracer(name).Start(ctx, "bootstrapRoles()")
+	defer span.End()
+
+	domains, err := client.Domains(ctx)
+	if err != nil {
+		return errors.Wrap(err, "UserManager.Domains()")
+	}
+
+	if err := removeUnusedRoles(ctx, domains, client, roles); err != nil {
+		return err
+	}
+
+	for _, r := range roles {
+		globalPermResources := make(map[accesstypes.Permission][]accesstypes.Resource)
+		domainPermResources := make(map[accesstypes.Permission][]accesstypes.Resource)
+		for perm, resources := range r.Permissions {
+			for _, resource := range resources {
+				if r := store.Scope(resource); r == "" {
+					return errors.Newf("resource %s does not require a permission or does not exist", resource)
+				} else if r == accesstypes.GlobalPermissionScope {
+					globalPermResources[perm] = append(globalPermResources[perm], resource)
+				} else {
+					domainPermResources[perm] = append(domainPermResources[perm], resource)
+				}
+			}
+		}
+
+		for _, domain := range domains {
+			if !client.RoleExists(ctx, domain, r.Name) {
+				if err := client.AddRole(ctx, domain, r.Name); err != nil {
+					return errors.Wrapf(err, "role %q to domain %s", r.Name, domain)
+				}
+				fmt.Printf("Added role %q to domain %s\n", r.Name, domain)
+			}
+
+			perms := globalPermResources
+			if domain != accesstypes.GlobalDomain {
+				perms = domainPermResources
+			}
+
+			existingPermissions, err := client.RolePermissions(ctx, domain, r.Name)
+			if err != nil {
+				return errors.Wrapf(err, "role %q to domain %s", r.Name, domain)
+			}
+
+			newPermissions := exclude(perms, existingPermissions)
+			for permission, resources := range newPermissions {
+				if err := client.AddRolePermissionResources(ctx, domain, r.Name, permission, resources...); err != nil {
+					return errors.Wrapf(err, "permissions %v, role %s", perms, r.Name)
+				}
+				fmt.Printf("Added Permissions %v to role %s and domain %s\n", newPermissions, r.Name, domain)
+			}
+
+			removePermissions := exclude(existingPermissions, perms)
+			for permission, resources := range removePermissions {
+				if err := client.DeleteRolePermissionResources(ctx, domain, r.Name, permission, resources...); err != nil {
+					return errors.Wrapf(err, "permissions %v, role %s", perms, r.Name)
+				}
+				fmt.Printf("Removed Permissions %v from role %s and domain %s\n", removePermissions, r.Name, domain)
+			}
+		}
+	}
+
+	return nil
+}
+
+func removeUnusedRoles(ctx context.Context, domains []accesstypes.Domain, client UserManager, newRoles []*Role) error {
+	for _, domain := range domains {
+		existingRoles, err := client.Roles(ctx, domain)
+		if err != nil {
+			return errors.Wrap(err, "client.Roles()")
+		}
+
+	EXISTING:
+		for _, er := range existingRoles {
+			for _, nr := range newRoles {
+				if nr.Name == er {
+					continue EXISTING
+				}
+			}
+			if _, err := client.DeleteRole(ctx, domain, er); err != nil {
+				return errors.Wrap(err, "client.DeleteRole()")
+			}
+			fmt.Printf("Removed old Role %s\n", er)
+		}
+	}
+
+	return nil
+}
+
+// exclude returns all elements that exist in source but not exclude
+func exclude(source, exclude map[accesstypes.Permission][]accesstypes.Resource) map[accesstypes.Permission][]accesstypes.Resource {
+	list := make(map[accesstypes.Permission][]accesstypes.Resource)
+
+	for sk, sv := range source {
+		ev := exclude[sk]
+		for _, item := range sv {
+			if slices.Contains(ev, item) {
+				continue
+			}
+			list[sk] = append(list[sk], item)
+		}
+	}
+
+	return list
+}
---
+++ migrate_test.go
@@ -0,0 +1,57 @@
+// deployment provides the utilities to bootstrap the application with preset configuration
+package access
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/cccteam/ccc/accesstypes"
+)
+
+func Test_exclude(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		source  map[accesstypes.Permission][]accesstypes.Resource
+		exclude map[accesstypes.Permission][]accesstypes.Resource
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[accesstypes.Permission][]accesstypes.Resource
+	}{
+		{
+			name: "has intersection",
+			args: args{
+				source:  map[accesstypes.Permission][]accesstypes.Resource{accesstypes.Permission("1"): {accesstypes.Resource("1"), accesstypes.Resource("2"), accesstypes.Resource("3"), accesstypes.Resource("4")}},
+				exclude: map[accesstypes.Permission][]accesstypes.Resource{accesstypes.Permission("1"): {accesstypes.Resource("2"), accesstypes.Resource("4")}},
+			},
+			want: map[accesstypes.Permission][]accesstypes.Resource{accesstypes.Permission("1"): {accesstypes.Resource("1"), accesstypes.Resource("3")}},
+		},
+		{
+			name: "has no intersection",
+			args: args{
+				source:  map[accesstypes.Permission][]accesstypes.Resource{accesstypes.Permission("1"): {accesstypes.Resource("1"), accesstypes.Resource("2"), accesstypes.Resource("3"), accesstypes.Resource("4")}},
+				exclude: map[accesstypes.Permission][]accesstypes.Resource{accesstypes.Permission("1"): {accesstypes.Resource("5"), accesstypes.Resource("6")}},
+			},
+			want: map[accesstypes.Permission][]accesstypes.Resource{accesstypes.Permission("1"): {accesstypes.Resource("1"), accesstypes.Resource("2"), accesstypes.Resource("3"), accesstypes.Resource("4")}},
+		},
+		{
+			name: "complete overlap",
+			args: args{
+				source:  map[accesstypes.Permission][]accesstypes.Resource{accesstypes.Permission("1"): {accesstypes.Resource("1"), accesstypes.Resource("2")}},
+				exclude: map[accesstypes.Permission][]accesstypes.Resource{accesstypes.Permission("1"): {accesstypes.Resource("1"), accesstypes.Resource("2")}},
+			},
+			want: map[accesstypes.Permission][]accesstypes.Resource{},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			if got := exclude(tt.args.source, tt.args.exclude); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Exclude() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
