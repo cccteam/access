5fe08323b13d00a1d9bbd526b21ce9a4e372a509
Joseph Watson <joseph@cloud-team.com>
2025-11-01 18:23:36 +0000

    Use mockgen go generate mocks

--- mock/mock.go
+++ mock/mock.go
@@ -2,6 +2,7 @@
 package mock
 
 //go:generate mockgen -package access -source ../access_iface.go -destination ../mock_access_iface.go
+//go:generate mockgen -package access -source ../store_iface.go -destination ../mock_store_iface.go
 //
 //go:generate mockgen -source ../access_iface.go -destination mock_access/mock_manager.go
 //go:generate mockgen -source ../handler.go -destination mock_access/mock_handler.go
